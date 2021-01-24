b5860c27d8e43da9b9c695d281db62cbdba50913
Stepan Yurchenko <stepan33314@yandex.ru>
2021-01-24 15:56:52 +0500

    Fix codestyle in worker test:

--- unit_tests/worker_test.py
+++ unit_tests/worker_test.py
@@ -1,10 +1,11 @@
+import socket as s
 import unittest
+from io import BytesIO
 from queue import Queue
-import socket
 from unittest.mock import patch
 
 from domain import ResultStruct
-from tcp_factory import TCPWorker, SocketFactory, TCPFactory
+from tcp_factory import TCPWorker, SocketFactory, TCPFactory, TCPWorkerFactory
 
 
 class WorkerTests(unittest.TestCase):
@@ -24,11 +25,8 @@ class WorkerTests(unittest.TestCase):
     def test_set_source(self):
         with patch.object(self.queue, attribute="get", return_value=23456):
             for v in self.data:
-                with patch.object(
-                    socket,
-                    attribute="gethostname",
-                    return_value="127.0.0.1" if v[3] == "localhost" else v[3],
-                ):
+                rv = "127.0.0.1" if v[3] == "localhost" else v[3]
+                with patch.object(s, "gethostname", return_value=rv):
                     worker = TCPWorker(
                         self.sockets,
                         self.packets,
@@ -41,6 +39,41 @@ class WorkerTests(unittest.TestCase):
                         self.queue,
                     )
                     self.assertEqual(worker.params[1], v[-1])
+
+
+class WorkTest(unittest.TestCase):
+    answers = {
+        b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x08\x00\x45\x00"
+        b"\x00\x28\x27\x10\x00\x00\xff\x06\x96\xbd\x7f\x00\x00\x01\x7f\x00"
+        b"\x00\x01\xb3\xa2\x15\x38\x00\x00\x00\x00\x00\x00\x00\x01\x50\x02"
+        b"\x00\x00\xe9\x04\x00\x00":
+        b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x08\x00\x45"
+        b"\x00\x00\x2c\x00\x00\x40\x00\x40\x06\x3c\xca\x7f\x00\x00\x01"
+        b"\x7f\x00\x00\x01\x15\x38\xb3\xa2\x67\x8e\xc4\x1a\x00\x00\x00"
+        b"\x01\x60\x12\xff\xd7\xfe\x20\x00\x00\x02\x04\xff\xd7"
+    }
+
+    def setUp(self) -> None:
+        resulter = ResultStruct(*ResultStruct.default_config())
+        self.factory = TCPWorkerFactory()
+        self.factory.set_resulter(resulter)
+        timeout, port, queue, seq = 2, 5432, Queue(), 0
+        k, host = "127.0.0.1", "127.0.0.1"
+        with patch.object(queue, "get", return_value=10000):
+            with patch.object(s, "gethostname", return_value=host):
+                worker = self.factory.make(timeout, k, host, port, queue, seq)
+        self.worker = worker
+
+    @patch("builtins.print")
+    @patch("socket.socket.sendto", new_callable=BytesIO)
+    def test_work(
+        self, mock_print, mock_sendto,
+    ):
+        for req, res in self.answers.items():
+            with patch.object(s.socket, attribute="recv", return_value=res):
+                self.worker._work()
+                self.assertEqual(req, mock_sendto.get_value())
+        self.assertTrue(mock_print.called)
 
 
 if __name__ == "__main__":
